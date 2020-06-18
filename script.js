efdddeee64232fe84eaeb025ea3fe52ba585b677
Calvin H. Lee <65202109+emailmecalvin128@users.noreply.github.com>
2020-06-18 14:04:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const  startingminutes= 10;
+let time =startingminutes =60; 
+
+const countdownEl= document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+const minutes= Math.floor(time/60);
+let seconds = time % 60);
+
+seconds= seconds <10 ? '0' + seconds: seconds; 
+
+
+    countdownEl.innerHTML =`${minutes}: ${seconds}`;
+    time--; 
+}
